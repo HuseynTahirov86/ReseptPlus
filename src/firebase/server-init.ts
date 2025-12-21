403ac2ca2f5af7f1c4da32d08f2a7269e611bf28
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2025-12-21 18:35:29 +0000

    node_modules_next_dist_compiled_react-dom_1e674e59._.js:sourcemap:14538

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -5,9 +5,6 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 let app: App;
 
-// In a server environment (like Firebase App Hosting), initializeApp() with no arguments
-// will automatically use the available service account and project configuration
-// from the environment variables.
 if (!getApps().length) {
     app = initializeApp();
 } else {
