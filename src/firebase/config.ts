c97f4656124730afae623544c0547c943e095551
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2025-12-21 16:43:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-498030416-70ea5",
+  "appId": "1:150295020220:web:a596244b2b120daa9446b9",
+  "apiKey": "AIzaSyAT1SdEaITNIdv4WvWXXsoSeTg3gy7MQos",
+  "authDomain": "studio-498030416-70ea5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "150295020220"
+};
