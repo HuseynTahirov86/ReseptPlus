a4bbf5d1f7ac2ad77cb9c6469b11ce72c82fac12
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2025-12-22 10:08:58 +0000

    Həkim əlavə edəndə heçnə olmur və əlavədə etmir. ONuda düzəlt

--- src/app/admin/hospitals/page.tsx
+++ src/app/admin/hospitals/page.tsx
@@ -70,11 +70,12 @@ export default function AdminHospitalsPage() {
     }
 
     const onFormSubmit = (state: { type: 'success' | 'error', message: string }) => {
+        toast({
+            title: state.type === 'success' ? 'Uğurlu' : 'Xəta',
+            description: state.message,
+            variant: state.type === 'success' ? 'default' : 'destructive',
+        });
         if (state.type === 'success') {
-            toast({
-                title: 'Uğurlu',
-                description: state.message,
-            });
             setIsFormOpen(false);
             setSelectedHospital(null);
         }
--- src/app/admin/pharmacists/page.tsx
+++ src/app/admin/pharmacists/page.tsx
@@ -84,11 +84,12 @@ export default function AdminPharmacistsPage() {
     }
 
     const onFormSubmit = (state: { type: 'success' | 'error', message: string }) => {
+        toast({
+            title: state.type === 'success' ? 'Uğurlu' : 'Xəta',
+            description: state.message,
+            variant: state.type === 'success' ? 'default' : 'destructive',
+        });
         if (state.type === 'success') {
-            toast({
-                title: 'Uğurlu',
-                description: state.message,
-            });
             setIsFormOpen(false);
             setSelectedPharmacist(null);
         }
