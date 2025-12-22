551c3d9134d803fc224296fa3e805653ced1eaee
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2025-12-22 07:32:42 +0000

    Yazıda olmasın, dediyim şəkildə həmin logo var. Şəkil, /logo.png-dir

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,8 @@ import { cn } from "@/lib/utils";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex items-center gap-2", className)}>
-      <Image src="/logo.png" alt="ReseptPlus Logo" width={28} height={28} />
-      <div className="text-2xl font-bold tracking-tight">
-        <span className="font-medium">Resept</span>
-        <span className="text-primary">Plus</span>
-      </div>
+    <div className={cn("flex items-center", className)}>
+      <Image src="/logo.png" alt="ReseptPlus Logo" width={140} height={35} priority />
     </div>
   );
 }
